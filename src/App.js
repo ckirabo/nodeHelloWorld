5a08a13113405a394c1750a36ed8578473fae9b8
clare <kirabo646@googlemail.com>
2019-02-06 13:58:26 +0000

    hello world complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './header.js';
+import Content from './content.js';
+
 
 class App extends Component {
+
+constructor(){
+  super();
+  this.state = {
+    header : "HELLO WORLD from Props",
+    content: "hello world from props"
+  }
+  this.function = () => this.setState({
+   header: "hello",
+   content:"WORLD"
+    });
+}
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+      <Header headerProp={this.state.header}/>
+      <Content contentProp={this.state.content}/>
+      <button onClick={this.function}>
+      Click Me </button>
       </div>
     );
   }
---
+++ src/content.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Content extends Component {
+render(){
+	return(
+		<div>
+         <h1>{this.props.contentProp}</h1>
+		</div>
+		);
+}
+
+
+}
+
+export default Content;
